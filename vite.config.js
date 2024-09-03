d2c0a18c2f11877d3f88a73cd64e11f4047f2f74
akashkumar8115 <akash2884182@gmail.com>
2024-09-03 21:06:01 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/user':'http://localhost:4001'
+  server: {
+    proxy: {
+      '/user': {
+        target: 'http://localhost:4001',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/user/, '')
+      }
     },
   },
   plugins: [react()],
